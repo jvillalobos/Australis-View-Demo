68d5f5707bbb3d19f8c050ba3cd5ab6cf4ac1377
Jorge Villalobos <jorge@xulforge.com>
2014-02-27 15:52:10 -0600

    Simple view with a Play button
    I need to hook it up to the content so it’ll show tracks in the content
    and allow the user to play them.

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -21,6 +21,7 @@ const Ci = Components.interfaces;
 const Cu = Components.utils;
 
 Cu.import("resource:///modules/CustomizableUI.jsm");
+Cu.import("resource://gre/modules/Services.jsm");
 
 var console =
   Cu.import("resource://gre/modules/devtools/Console.jsm", {}).console;
@@ -39,6 +40,7 @@ function shutdown(aData, aReason) {
 
 let AusView = {
   init : function() {
+    let enumerator = Services.wm.getEnumerator("navigator:browser");
     let io =
       Cc["@mozilla.org/network/io-service;1"].
         getService(Ci.nsIIOService);
@@ -51,25 +53,96 @@ let AusView = {
     this._uri = io.newURI("chrome://aus-view/skin/toolbar.css", null, null);
     this._ss.loadAndRegisterSheet(this._uri, this._ss.USER_SHEET);
 
+    while (enumerator.hasMoreElements()) {
+      this.windowListener.addUI(enumerator.getNext());
+    }
+
+    Services.wm.addListener(this.windowListener);
+
     // create widget and add it to the main toolbar.
     CustomizableUI.createWidget(
-      { id : "aus-hello-button",
+      { id : "aus-view-button",
+        type : "view",
+        viewId : "aus-view-panel",
         defaultArea : CustomizableUI.AREA_NAVBAR,
         label : "Hello Button",
         tooltiptext : "Hello!",
-        onCommand : function(aEvent) {
-          let win = aEvent.target.ownerDocument.defaultView;
-
-          win.alert("Hello!");
+        onViewShowing : function (aEvent) {
+          let doc = aEvent.target.ownerDocument;
+          let iframe = doc.getElementById("aus-view-iframe");
+        },
+        onViewHiding : function (aEvent) {
+          let doc = aEvent.target.ownerDocument;
+          let iframe = doc.getElementById("aus-view-iframe");
         }
       });
   },
 
   uninit : function() {
-    CustomizableUI.destroyWidget("aus-hello-button");
+    let enumerator = Services.wm.getEnumerator("navigator:browser");
+
+    CustomizableUI.destroyWidget("aus-view-button");
+
+    Services.wm.removeListener(this.windowListener);
+
+    while (enumerator.hasMoreElements()) {
+      this.windowListener.removeUI(enumerator.getNext());
+    }
 
     if (this._ss.sheetRegistered(this._uri, this._ss.USER_SHEET)) {
       this._ss.unregisterSheet(this._uri, this._ss.USER_SHEET);
     }
+  },
+
+  windowListener : {
+    /**
+     * Adds the panel view for the button on all windows.
+     */
+    addUI : function(aWindow) {
+      let doc = aWindow.document;
+      let panel = doc.createElement("panelview");
+      let iframe = doc.createElement("iframe");
+
+      panel.setAttribute("id", "aus-view-panel");
+      iframe.setAttribute("id", "aus-view-iframe");
+      iframe.setAttribute("type", "content");
+      iframe.setAttribute("src", "chrome://aus-view/content/player.html");
+
+      panel.appendChild(iframe);
+      doc.documentElement.appendChild(panel);
+    },
+
+    /**
+     * Removes all added UI elements.
+     */
+    removeUI : function(aWindow) {
+      let doc = aWindow.document;
+      let panel = doc.getElementById("aus-view-panel");
+
+      panel.parentNode.removeChild(panel);
+    },
+
+    onOpenWindow : function(aXULWindow) {
+      // A new window has opened.
+      let that = this;
+      let domWindow =
+        aXULWindow.QueryInterface(Ci.nsIInterfaceRequestor).
+        getInterface(Ci.nsIDOMWindow);
+
+      // Wait for it to finish loading
+      domWindow.addEventListener(
+        "load",
+        function listener() {
+          domWindow.removeEventListener("load", listener, false);
+          // If this is a browser window then setup its UI
+          if (domWindow.document.documentElement.getAttribute("windowtype") ==
+              "navigator:browser") {
+            that.addUI(domWindow);
+          }
+      }, false);
+    },
+
+    onCloseWindow : function(aXULWindow) {},
+    onWindowTitleChange: function(aXULWindow, aNewTitle) {}
   }
 };
